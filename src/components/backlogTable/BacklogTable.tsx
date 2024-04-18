5f8b9d6e96c6ef30dd1d69e5ec7c6ceb3bc128fc
Ptrk12 <random12@onet.pl>
2024-04-18 11:37:55 +0200

    finished backlog component

--- src/components/backlogTable/BacklogTable.tsx
+++ src/components/backlogTable/BacklogTable.tsx
@@ -1,57 +1,168 @@
-import React from 'react'
-import "./backlogTable.scss"
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-import TaskType from '../../types/TaskType'
+import "./backlogTable.scss";
+import {
+  Button,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import TaskType from "../../types/TaskType";
+import State from "../../enums/State";
+import { useState } from "react";
 
+const BacklogTable = (props: { props: TaskType[] }) => {
+  const tasks = props.props;
+  let { projectId } = useParams();
+  const [state, setState] = useState(State.ToDo);
+  const toDoTasks = tasks.filter((x) => x.state === State.ToDo);
+  const doingTasks = tasks.filter((x) => x.state === State.Doing);
+  const doneTasks = tasks.filter((x) => x.state === State.Done);
 
-const BacklogTable = (props:{props:TaskType}) => {
+  const handleChange = (event: SelectChangeEvent, taskId: number) => {
+    const taskToUpdate = tasks.find((x: TaskType) => x.id === taskId);
+    const newState = event.target.value as State; 
+    setState(newState);
+  
+    if (taskToUpdate != undefined) {
+      taskToUpdate.state = newState; 
+      console.log(newState); 
+      localStorage.setItem(taskId.toString(), JSON.stringify(taskToUpdate));
+      window.location.reload(); 
+    }
+  };
+  
+
   return (
-    <div className='backlogTable'>
+    <div className="backlogTable">
       <div className="toDo">
         <div className="title">
-          <h3 className='colorTitle'>
-            ToDo
-          </h3>
-          <div className='item'>
-            <div className="info">
-              <div className="name">
-                <b>Task name:</b> TASK1
+          <h3 className="colorTitle">ToDo</h3>
+          {toDoTasks.map((task) => (
+            <div className="item" key={task.id}>
+              <div className="info">
+                <div className="name">
+                  <b>Task name:</b> {task.name}
+                </div>
+                <div className="desc">
+                  <b>Description:</b> {task.description}
+                </div>
+                <div className="state">
+                  <b>State:</b> {task.state}
+                </div>
               </div>
-              <div className="desc">
-                <b>Description:</b> TASK DESC
+              <div className="assigner">
+                <b>Assigner:</b> {task.assigner}
               </div>
-              <div className="state">
-                <b>State:</b> ToDo
+              <div className="button">
+                <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={task.state}
+                  label="Age"
+                  onChange={(event) => handleChange(event, task.id)}
+                >
+                  <MenuItem value={State.ToDo}>ToDo</MenuItem>
+                  <MenuItem value={State.Doing}>Doing</MenuItem>
+                  <MenuItem value={State.Done}>Done</MenuItem>
+                </Select>
+                <Link
+                  to={`/projects/${projectId}/userstory/${task.userStoryId}/createtask?taskId=${task.id}`}
+                >
+                  <Button variant="outlined">Details</Button>
+                </Link>
               </div>
             </div>
-            <div className="assigner">
-              <b>Assigner:</b> Patryk
-            </div>
-            <div className="button">
-              <Link to={"s"}>
-                <Button variant="outlined">Details</Button>
-              </Link>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="doing">
         <div className="title">
-          <h3 className='colorTitle'>
-            Doing
-          </h3>
+          <h3 className="colorTitle">Doing</h3>
+          {doingTasks.map((task) => (
+            <div className="item" key={task.id}>
+              <div className="info">
+                <div className="name">
+                  <b>Task name:</b> {task.name}
+                </div>
+                <div className="desc">
+                  <b>Description:</b> {task.description}
+                </div>
+                <div className="state">
+                  <b>State:</b> {task.state}
+                </div>
+              </div>
+              <div className="assigner">
+                <b>Assigner:</b> {task.assigner}
+              </div>
+              <div className="button">
+                <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={task.state}
+                  label="Age"
+                  onChange={(event) => handleChange(event, task.id)}
+                >
+                  <MenuItem value={State.ToDo}>ToDo</MenuItem>
+                  <MenuItem value={State.Doing}>Doing</MenuItem>
+                  <MenuItem value={State.Done}>Done</MenuItem>
+                </Select>
+                <Link
+                  to={`/projects/${projectId}/userstory/${task.userStoryId}/createtask?taskId=${task.id}`}
+                >
+                  <Button variant="outlined">Details</Button>
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <div className="done">
         <div className="title">
-          <h3 className='colorTitle'>
-            Done
-          </h3>
+          <h3 className="colorTitle">Done</h3>
+          {doneTasks.map((task) => (
+            <div className="item" key={task.id}>
+              <div className="info">
+                <div className="name">
+                  <b>Task name:</b> {task.name}
+                </div>
+                <div className="desc">
+                  <b>Description:</b> {task.description}
+                </div>
+                <div className="state">
+                  <b>State:</b> {task.state}
+                </div>
+              </div>
+              <div className="assigner">
+                <b>Assigner:</b> {task.assigner}
+              </div>
+              <div className="button">
+                <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={task.state}
+                  label="Age"
+                  onChange={(event) => handleChange(event, task.id)}
+                >
+                  <MenuItem value={State.ToDo}>ToDo</MenuItem>
+                  <MenuItem value={State.Doing}>Doing</MenuItem>
+                  <MenuItem value={State.Done}>Done</MenuItem>
+                </Select>
+                <Link
+                  to={`/projects/${projectId}/userstory/${task.userStoryId}/createtask?taskId=${task.id}`}
+                >
+                  <Button variant="outlined">Details</Button>
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BacklogTable
+export default BacklogTable;
--- src/pages/Backlog/Backlog.tsx
+++ src/pages/Backlog/Backlog.tsx
@@ -1,30 +1,23 @@
-import React from 'react'
-import BacklogTable from '../../components/backlogTable/BacklogTable'
-import TaskType from '../../types/TaskType'
-import Priority from '../../enums/Priority'
-import State from '../../enums/State'
+import React from "react";
+import BacklogTable from "../../components/backlogTable/BacklogTable";
+import TaskType from "../../types/TaskType";
+import Priority from "../../enums/Priority";
+import State from "../../enums/State";
+import { localStorageWorker } from "../../storage/localStorageWorker";
+import { useParams } from "react-router-dom";
 
-
-const test:TaskType ={
-  id:1,
-  name:"Task1",
-  description:"description",
-  projectName:"Project 1",
-  workHours:2,
-  userStoryId:"12",
-  projectId:"12",
-  priority: Priority.High,
-  state: State.ToDo,
-  assigner:"Patryk",
-  type:"task"
-}
 
 const Backlog = () => {
+  let { projectId } = useParams();
+
+  const data = localStorageWorker.getAllItems();
+  let tasks = data.filter((x:TaskType) => x.type === "task" && x.projectId === projectId?.toString());
+
   return (
     <div>
-      <BacklogTable props={test} />
+      <BacklogTable props={tasks} />
     </div>
-  )
-}
+  );
+};
 
-export default Backlog
+export default Backlog;
